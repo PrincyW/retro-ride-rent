eb6daef096c3d47703013925460f9cc87a96c34d
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-27 17:39:17 +0100

    Added has_many bookings for car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ApplicationRecord
   CITIES = ["Paris", "Lyon", "Lille", "Marseille"]
   belongs_to :user
   has_many :users, through: :bookings
+  has_many :bookings, dependent: :destroy
   validates :brand, presence: true
   validates :photo_url, presence: true
   validates :price, presence: true
