37f45d7959a032fa325f1153fe350bbcf38bebd8
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-27 17:23:06 +0100

    Correcting footer position, creating modal for form

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  connect() {
-    console.log("Hello from toggle_controller.js")
-  }
+  static targets = ["togglableForm"]
+
+  fire() {
+  this.togglableFormTarget.classList.toggle("d-none");
+}
 }
