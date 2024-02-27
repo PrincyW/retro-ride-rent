3ffb370be32aa9fd02f2b34360a32c2ef18fadaf
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-27 13:40:29 +0100

    Adding show for cars, adding CSS style for individual car display

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,19 @@
 class CarsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
+  before_action :set_car, only: [:show]
+
   def index
     @cars = Car.all
   end
+
+  def show
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
 end
